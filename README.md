f453a4fcdd0b3e3ba564949529cd2d108af29c92
oliver2ba <oliver.belliardabreu@gmail.com>
2025-01-24 12:22:58 +0100

    Demo code and README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MotorcortexApplication
- A Motorcortex experiment with a Robot App and a Generic App.
+ An easy and quick Motorcortex experiment to pair with a local (intel) Robot App. The goal here is to learn how to use the Python API paired with the a robot simulation.
+
+## Source
+
+The project is meant to be used with the browser of your choice and a Visual Studio Code environment.
+- To set up your VS Code environment: https://docs.motorcortex.io/docs/developing-control-applications/environment-setup/
+- To set up the Virtual machine and accessible through a DHCP server: https://docs.motorcortex.io/docs/getting-started/connect-your-pc/configure-your-network/
+- To deploy the App, accessible through the [Motorcortex portal](https://app.motorcortex.io/projects/): https://docs.motorcortex.io/docs/getting-started/deploy-app/
+- To get started with the Python API and reproduce the content of this repo: https://docs.motorcortex.io/docs/developing-client-applications/python/
--- main.py
+++ main.py
@@ -1,41 +1,72 @@
 # import the motorcortex library
 import motorcortex
+import time
 
-# Create a parameter tree object
+
 parameter_tree = motorcortex.ParameterTree()
 certificate_path = "mcx.cert.crt" #"~/Documents/Vectioneer/MotorcortexApplication/mcx.cert.crt"
 
-# Open request and subscribe connection
-try:
-  req, sub = motorcortex.connect(url="wss://192.168.56.101:5568:5567",
-                                 motorcortex_types=motorcortex.MessageTypes(), param_tree=parameter_tree,
-                                 certificate=certificate_path,
-                                 timeout_ms=1000,
-                                 login="root", password="secret")
-  tree = parameter_tree.getParameterTree()
-  print(f"Parameters: {tree}")
+# Define the callback function that will be called whenever a message is received
+def message_received(parameters):
+  for cnt in range(0, len(parameters)):
+    param = parameters[cnt]
+    timestamp = param.timestamp.sec + param.timestamp.nsec * 1e-9
+    value = param.value
+    # print the timestamp and value; convert the value to a string first
+    # so we do not need to check all types before printing it
+    print(f"Notify: {timestamp}, {value}")
 
-  # Requesting the parameter tree
-  param_tree_reply = req.getParameterTree()
-  param_tree_reply_msg = param_tree_reply.get(0)
-  if param_tree_reply_msg:
-    print("Parameter tree received")
+
+def main():
+  # Create a parameter tree object
+  parameter_tree = motorcortex.ParameterTree()
+  # Open request and subscribe connection
+  try:
+    req, sub = motorcortex.connect(url="wss://192.168.56.101:5568:5567",
+                              motorcortex_types=motorcortex.MessageTypes(), param_tree=parameter_tree,
+                              certificate=certificate_path,
+                              timeout_ms=1000,
+                              login="root", password="secret")
+    tree = parameter_tree.getParameterTree()
+    print(f"Parameters: {tree}")
+
+  except RuntimeError as err:
+    print(err)
+    exit()
+
+  paths = ['root/Control/hostInJointVelocity']
+  # define the frequency divider that tells the server to publish only every
+  # n-th sample. This depends on the update rate of the publisher.
+  divider = 100
+  # subscribe and wait for the reply with a timeout of 10 seconds
+  subscription = sub.subscribe(paths, 'group1', divider)
+  # get reply from the server
+  is_subscribed = subscription.get()
+  # print subscription status and layout
+  if (is_subscribed is not None) and (is_subscribed.status == motorcortex.OK):
+    print(f"Subscription successful, layout: {subscription.layout()}")
   else:
-    print("Failed to receive parameter tree")
-  parameter_tree.load(param_tree_reply_msg)
-
-  # Gripper control test
-  get_param_reply_msg = req.getParameter('root/UserParameters/IO/Gripper').get()
-  print("nget_param_reply_msg:")
-  print(get_param_reply_msg)
-
-  set_param_reply_msg = req.setParameter('root/UserParameters/IO/Gripper',
-  True).get()
-  print("nset_param_reply_msg:")
-  print(set_param_reply_msg)
+    print(f"Subscription failed, do your paths exist? \npaths: {paths}")
+    sub.close()
+    exit()
+    
+  # set the callback function that handles the received data
+  # Note that this is a non-blocking call, starting a new thread that handles
+  # the messages. You should keep the application alive for a s long as you need to
+  # receive the messages
+  subscription.notify(message_received)
   
-  # Close connection for fresh restart
+  # polling subscription
+  for i in range(100):
+    value = subscription.read()
+    if value:
+      print(f"Polling, timestamp: {value[0].timestamp} value: {value[0].value}")
+    time.sleep(1)
+
+  # close the subscription when done
+  sub.close()
+  # Close connection for a fresh restart
   req.close()
 
-except RuntimeError as err:
-  print(err)
+if __name__ == "__main__":
+  main()
