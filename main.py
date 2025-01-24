dd3b68bf56697a26bd714a67a1d7b8228ef2f7a1
oliver2ba <oliver.belliardabreu@gmail.com>
2025-01-24 02:49:42 +0100

    Check if the parameter tree has been well received

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 # import the motorcortex library
 import motorcortex
+
 # Create a parameter tree object
 parameter_tree = motorcortex.ParameterTree()
+certificate_path = "mcx.cert.crt" #"~/Documents/Vectioneer/MotorcortexApplication/mcx.cert.crt"
+
 # Open request and subscribe connection
 try:
-  req, sub = motorcortex.connect("wss://192.168.56.101:5568:5567", motorcortex.MessageTypes(), parameter_tree,
-                                   certificate="mcx.cert.crt", timeout_ms=1000,
-                                   login="root", password="secret")
+  req, sub = motorcortex.connect(url="wss://192.168.56.101:5568:5567",
+                                 motorcortex_types=motorcortex.MessageTypes(), param_tree=parameter_tree,
+                                 certificate=certificate_path,
+                                 timeout_ms=1000,
+                                 login="root", password="secret")
   tree = parameter_tree.getParameterTree()
   print(f"Parameters: {tree}")
 
+  # Requesting the parameter tree
+  param_tree_reply = req.getParameterTree()
+  param_tree_reply_msg = param_tree_reply.get(0)
+  if param_tree_reply_msg:
+    print("Parameter tree received")
+  else:
+    print("Failed to receive parameter tree")
+  parameter_tree.load(param_tree_reply_msg)
+
+
 except RuntimeError as err:
-    print(err)
+  print(err)
